b4d044f39a82453657d20a6ac621a9b0eff91d73
Joao Arthur Brunet Monteiro <joaoarthurbm@gmail.com>
2024-04-17 17:10:29 -0300

    dump

---
+++ src/main/java/Heap.java
@@ -0,0 +1,120 @@
+import java.util.Arrays;
+
+public class Heap {
+    
+    private int[] heap;
+    private int tail;
+    
+    public Heap(int capacidade) {
+        this.heap = new int[capacidade];
+        this.tail = -1;
+    }
+    
+    public Heap(int[] heap) {
+        this.heap = heap;
+        this.tail = this.heap.length - 1;
+        this.buildHeap();
+    }
+
+    public boolean isEmpty() {
+        return this.tail == -1;
+    }
+
+    public int left(int i) {
+    	// TODO
+        return -1;
+    }
+
+    public int right(int i) {
+        // TODO
+        return -1;
+    }
+
+    public int parent(int i) {
+        // TODO
+        return -1;
+    }
+
+    public void add(int n) {
+    	// TODO
+    }
+
+    private void buildHeap() {
+        // TODO 
+    }
+    
+    public int remove() {
+        if (isEmpty()) throw new RuntimeException("Empty");
+        int element = this.heap[0];
+        this.heap[0] = this.heap[tail];
+        this.tail -= 1;
+
+        this.heapify(0);
+        
+        return element;
+    }
+        
+    private void heapify(int index) {
+        if (isLeaf(index) || !isValidIndex(index)) 
+            return;
+        
+        // compares index, left and right to find max
+        int index_max = max_index(index, left(index), right(index));
+        
+        // if current index is not greater than its children, 
+        // swap and keep heapifying.
+        if (index_max != index) {
+                swap(index, index_max);
+                heapify(index_max);
+        }
+    } 
+    
+    private int max_index(int index, int left, int right) {
+        if (this.heap[index] > this.heap[left]) {
+            if (isValidIndex(right)) {
+                if (this.heap[index] < this.heap[right])
+                    return right;
+            }
+            return index;
+        
+        } else {
+            if (isValidIndex(right)) {
+                if (this.heap[left] < this.heap[right])
+                    return right;
+            } 
+            
+            return left;
+        }
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= tail;
+    }
+    
+    private boolean isLeaf(int index) {
+        return index > parent(tail) && index <= tail; 
+    } 
+    
+    private void swap(int i, int j) {
+        int aux = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = aux;
+    }
+
+    private void resize() {
+        int[] novoHeap = new int[this.heap.length * 2];
+        for (int i = 0; i <= tail; i++)
+            novoHeap[i] = this.heap[i];
+        
+        this.heap = novoHeap;
+    }
+    
+    public int size() {
+        return this.tail + 1;
+    }
+    
+    public String toString() {
+        return Arrays.toString(this.heap);
+    }
+
+}
